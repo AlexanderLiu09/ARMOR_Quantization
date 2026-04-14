2eb27b7826520924492416b63c84011c12faba25
HazeGrayUnderway <alexander.liu.us@gmail.com>
2026-04-14 15:54:40 -0700

    bash scripts updated

--- scripts/compress/ARMOR_quantized.bash
+++ scripts/compress/ARMOR_quantized.bash
@@ -10,12 +10,13 @@ declare gpus=4,5,6,7 #change this for your machine
 declare run_name="_run_name"
 declare model="google/gemma-2-9b"
 declare dataset_config="[{dataset_config:SlimPajama-627B,n_samples:128,ctx_len:8192}]"
-declare block_size=128 #default block size for BlockPrune
-declare n_iters=5000 #default number of iterations for BlockPrune
+declare block_size=128 #default block size for block-diagonal A/B
+declare n_iters=5000 #default number of iterations
 declare additional_args=""
-declare quant_n_bits=4
-declare quant_n_grid=100
-declare quant_start=500
+declare quant_n_bits=4 #INT precision for the quantized sparse core
+declare groupsize=128 #quantization groupsize (must divide d_in and be a multiple of sparse group)
+declare n_sparse_core_updates_per_iter=0 #discrete (mask, q) sweeps per iter; 0 = STE-only
+declare sparse_core_step_select="gradient_all_random" #group selection strategy
 
 #loop through the args
 for arg in "$@"
@@ -49,11 +50,11 @@ cmd="CUDA_VISIBLE_DEVICES=${gpus} python -u ParallelCompress.py \
     compress=ARMOR_quantized \
     run_name=$run_name \
     compress.compression_config.block_diagonal_config.block_size=$block_size \
-    +compress.compression_config.naive_compression_config.compression_config.pattern=[2,4] \
+    compress.compression_config.naive_compression_config.compression_config.quant_precision=$quant_n_bits \
+    compress.compression_config.naive_compression_config.compression_config.groupsize=$groupsize \
     compress.compression_config.training_config.n_iters=$n_iters \
-    compress.compression_config.training_config.quant_n_bits=$quant_n_bits \
-    compress.compression_config.training_config.quant_n_grid=$quant_n_grid \
-    compress.compression_config.training_config.quant_qat_start_iter=$quant_start
+    compress.compression_config.training_config.n_sparse_core_updates_per_iter=$n_sparse_core_updates_per_iter \
+    compress.compression_config.training_config.sparse_core_step_select=$sparse_core_step_select \
      \"datasets=${dataset_config}\""
 
 #split the additional args by space and add them to the command
@@ -65,7 +66,7 @@ done
 echo "Command to run: $cmd"
 
 
-    
+
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate $enviroment
 eval $cmd
